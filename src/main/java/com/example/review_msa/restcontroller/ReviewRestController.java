6105e5899897271f852056d530272f7d81fcd7d1
jonghechoi <sosinnmi@naver.com>
2023-08-11 18:40:53 +0900

    CORS 컨피그 추가. 아직 CORS 정책이 제대로 반영되지는 않아서 계속 작업 필요

--- src/main/java/com/example/review_msa/restcontroller/ReviewRestController.java
+++ src/main/java/com/example/review_msa/restcontroller/ReviewRestController.java
@@ -3,6 +3,7 @@ package com.example.review_msa.restcontroller;
 import com.example.review_msa.dto.ReviewDto;
 import com.example.review_msa.service.ReviewServiceImpl;
 import lombok.AllArgsConstructor;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -16,6 +17,7 @@ public class ReviewRestController {
     /**
      *	Index review
      */
+    @CrossOrigin(origins = "*")
     @GetMapping("index_review")
     public List<ReviewDto> index_review() {
         List<ReviewDto> list = reviewService.reviewIndex();
